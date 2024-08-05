df51026ce6887689964b65e525962339c1df4710
abdul <abdul.tanveer@gmail.com>
2024-08-05 19:02:10 +0530

    builder design pattern

--- cognizantrever/src/threads/TestSynchronization1.java
+++ cognizantrever/src/threads/TestSynchronization1.java
@@ -2,8 +2,8 @@ package threads;
 class TestSynchronization1{  
 	public static void main(String args[]){  
 		Table obj = new Table();//only one shared object  
-		MyThread1 t1=new MyThread1(obj);  
-		MyThread2 t2=new MyThread2(obj);  
+		MyThread1 t1 = new MyThread1(obj);  
+		MyThread2 t2 = new MyThread2(obj);  
 		t1.start();  
 		t2.start();  
 	}  
